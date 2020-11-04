7db53810a5550d6b719378b3600be9dd3775e97a
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-11-04 16:24:56 -0300

    header and banner of the 1st project page

--- src/pages/Project-one/index.tsx
+++ src/pages/Project-one/index.tsx
@@ -1,11 +1,74 @@
-import { FC } from 'react'
+import { FC, FormEvent } from 'react'
 
-import { Container } from './styles'
+import {
+  Container,
+  Header,
+  Logo,
+  Banner,
+  InfoBox,
+  Form,
+  InfoBoxList,
+  InfoBoxListItem,
+  FormTitle,
+  InputBlock,
+  Label,
+  Span,
+  Input,
+  Info,
+  SubmitButton
+} from './styles'
+
+import logo from '../../assets/logo.png'
 
 const ProjectOne: FC = () => {
+  function handleFormSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    alert('Parabéns você perderu 10 segundos de vida')
+  }
+
   return (
     <Container>
-      <h1>Project-one</h1>
+      <Header>
+        <Logo src={logo} alt="Danki Code" />
+      </Header>
+      <Banner>
+        <InfoBox>
+          <InfoBoxList>
+            <InfoBoxListItem>1° item da lista</InfoBoxListItem>
+            <InfoBoxListItem>2° item da lista</InfoBoxListItem>
+            <InfoBoxListItem>3° item da lista</InfoBoxListItem>
+            <InfoBoxListItem>4° item da lista</InfoBoxListItem>
+          </InfoBoxList>
+        </InfoBox>
+        <Form onSubmit={handleFormSubmit}>
+          <FormTitle>
+            Preencha o formulário e inicie a sua caminhada para virar um
+            webmaster!
+          </FormTitle>
+
+          <InputBlock>
+            <Label htmlFor="name">
+              Nome<Span>*</Span>
+            </Label>
+            <Input id="name" type="text" required />
+          </InputBlock>
+          <InputBlock>
+            <Label htmlFor="email">
+              E-mail<Span>*</Span>
+            </Label>
+            <Input id="email" type="email" required />
+          </InputBlock>
+          <InputBlock>
+            <Label htmlFor="telefone">
+              Telefone<Span>*</Span>
+            </Label>
+            <Input id="telefone" type="text" required />
+          </InputBlock>
+          <Info>* Campos obrigatórios</Info>
+          <SubmitButton type="submit">Enviar</SubmitButton>
+        </Form>
+      </Banner>
     </Container>
   )
 }
--- src/pages/Project-one/styles.ts
+++ src/pages/Project-one/styles.ts
@@ -1,3 +1,130 @@
 import styled from 'styled-components'
+import backgroundImage from '../../assets/header_bg.png'
 
 export const Container = styled.div``
+
+export const Header = styled.header`
+  height: 70px;
+  padding: 24px;
+  background-color: #212343;
+
+  display: flex;
+  align-items: center;
+`
+
+export const Logo = styled.img``
+
+export const Banner = styled.div`
+  height: 80vh;
+  padding: 48px;
+  background-image: url(${backgroundImage});
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const InfoBox = styled.div`
+  height: 250px;
+  width: 500px;
+
+  background-color: rgba(33, 35, 67, 0.7);
+  color: #fff;
+
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+`
+
+export const InfoBoxList = styled.ul`
+  width: 100%;
+  height: 100%;
+
+  padding: 32px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  li + li {
+    margin-top: 12px;
+  }
+`
+
+export const InfoBoxListItem = styled.li``
+
+export const Form = styled.form`
+  width: 400px;
+  height: 85vh;
+  padding: 16px;
+
+  background-color: #fff;
+
+  border: 12px solid rgba(120, 120, 120, 0.5);
+  background-clip: padding-box;
+
+  @media (max-width: 400px) {
+    width: 100%;
+  }
+`
+
+export const FormTitle = styled.h2`
+  font-weight: 800;
+  color: #999999;
+
+  font-size: 20px;
+  margin-bottom: 24px;
+`
+
+export const InputBlock = styled.div`
+  margin-bottom: 20px;
+`
+
+export const Label = styled.label`
+  color: #bbbbbb;
+  cursor: pointer;
+`
+
+export const Span = styled.span`
+  font-size: 14px;
+`
+
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+
+  padding-left: 8px;
+  font-size: 17px;
+
+  border: 1px solid #ccc;
+`
+
+export const Info = styled.span`
+  color: #bbbbbb;
+  font-size: 16px;
+`
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  height: 56px;
+
+  margin-top: 20px;
+
+  font-size: 25px;
+
+  color: #fff;
+  background-color: #363a6d;
+  border-bottom: 5px solid #4a5097;
+
+  border-top: 0;
+  border-right: 0;
+  border-left: 0;
+
+  cursor: pointer;
+
+  transition: background-color 400ms;
+
+  &:hover {
+    background-color: #323665;
+  }
+`
