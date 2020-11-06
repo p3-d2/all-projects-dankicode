3bc3a083bb3e16464c129c21e919582999a50d9b
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-11-06 14:04:49 -0300

    creating and styling the header and part of the main project facebook login page clone

--- src/pages/FacebookClone/index.tsx
+++ src/pages/FacebookClone/index.tsx
@@ -1,13 +1,67 @@
-import { FC } from 'react'
+import { FC, FormEvent } from 'react'
 import GoBack from '../../components/GoBack'
 
-import { Container } from './styles'
+import {
+  Container,
+  Header,
+  Logo,
+  FormHeader,
+  InputBlock,
+  Label,
+  Input,
+  SubmitButtonHeader,
+  Main,
+  MapSection,
+  Description,
+  SubTitle,
+  MapImg,
+  FormSection,
+  Title,
+  Form
+} from './styles'
+
+import mapImg from '../../assets/facebookClone/mapImg.png'
 
 const FacebookClone: FC = () => {
+  function handleSubmitForm(event: FormEvent) {
+    event.preventDefault()
+  }
+
   return (
     <Container>
       <GoBack />
-      <h1>Facebook-clone</h1>
+      <Header>
+        <Logo>Facebook</Logo>
+        <FormHeader onSubmit={handleSubmitForm}>
+          <InputBlock>
+            <Label htmlFor="emailOrTelephone">Email ou Telefone:</Label>
+            <Input id="emailOrTelephone" type="email" required />
+          </InputBlock>
+          <InputBlock>
+            <Label htmlFor="password">Senha:</Label>
+            <Input id="password" type="password" required />
+          </InputBlock>
+          <InputBlock>
+            <Label className="invisible">invisible</Label>
+            <SubmitButtonHeader type="submit">Entrar</SubmitButtonHeader>
+          </InputBlock>
+        </FormHeader>
+      </Header>
+
+      <Main>
+        <MapSection>
+          <Description>
+            O Facebook ajuda você a se conectar e compartilhar com as pessoas
+            que fazem parte da sua vida.
+          </Description>
+          <MapImg src={mapImg} alt="map" />
+        </MapSection>
+        <FormSection>
+          <Title>Abra sua conta</Title>
+          <SubTitle>É rápido e fácil</SubTitle>
+          <Form></Form>
+        </FormSection>
+      </Main>
     </Container>
   )
 }
--- src/pages/FacebookClone/styles.ts
+++ src/pages/FacebookClone/styles.ts
@@ -1,3 +1,111 @@
 import styled from 'styled-components'
 
 export const Container = styled.div``
+
+export const Header = styled.header`
+  height: 80px;
+  padding: 0 64px;
+
+  background: linear-gradient(to bottom, #3b5998, #3b5998);
+  color: #fff;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Logo = styled.h1`
+  font-size: 32px;
+  font-weight: 800;
+`
+
+export const FormHeader = styled.form`
+  display: flex;
+  align-items: center;
+`
+
+export const InputBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 0 8px;
+`
+
+export const Label = styled.label`
+  font-size: 14px;
+  margin-bottom: 4px;
+
+  &.invisible {
+    opacity: 0;
+  }
+`
+
+export const Input = styled.input`
+  height: 28px;
+  width: 156px;
+
+  font-size: 16px;
+
+  padding: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.6);
+`
+
+export const SubmitButtonHeader = styled.button`
+  height: 28px;
+  padding: 3px 5px;
+
+  background-color: #3a6cd6;
+  color: #fff;
+
+  border: 1px solid rgba(0, 0, 0, 0.3);
+
+  font-size: 15px;
+
+  cursor: pointer;
+
+  transition: 300ms background-color;
+
+  &:hover {
+    background-color: #1f4ba6;
+  }
+`
+
+export const Main = styled.main`
+  display: flex;
+  align-items: flex-start;
+
+  padding: 24px 64px 0;
+
+  background: linear-gradient(to bottom, #fff, #d3d8e8);
+`
+
+export const MapSection = styled.section`
+  width: 100%;
+  height: 100%;
+
+  margin-right: 16px;
+`
+
+export const Description = styled.h4``
+
+export const MapImg = styled.img`
+  margin: 12px 0;
+  width: 600px;
+`
+
+export const FormSection = styled.section`
+  width: 100%;
+  height: 100%;
+`
+
+export const Title = styled.h2`
+  font-size: 34px;
+  font-weight: 800;
+`
+
+export const SubTitle = styled.h3`
+  font-size: 24px;
+  margin-top: -8px;
+`
+
+export const Form = styled.form``
