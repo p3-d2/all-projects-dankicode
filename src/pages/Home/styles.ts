7acfb2ac8fe195aae06ded1b69e2dad150375ad9
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-11-06 12:52:13 -0300

    creating responsive design at home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,16 +7,28 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  overflow-x: hidden;
+
+  @media (max-width: 550px) {
+    height: 100%;
+    width: 100%;
+  }
 `
 
 export const Wrapper = styled.main`
-  width: max(60%, 400px);
-  height: max(75%, 400px);
+  height: 80%;
+  width: 65%;
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+
+  @media (max-width: 550px) {
+    height: 100%;
+    width: 100%;
+  }
 `
 
 export const Title = styled.h1`
@@ -24,6 +36,10 @@ export const Title = styled.h1`
   font-weight: 800;
 
   margin-bottom: 48px;
+
+  @media (max-width: 550px) {
+    margin: 48px 0;
+  }
 `
 
 export const ProjectsContainer = styled.div`
@@ -34,4 +50,13 @@ export const ProjectsContainer = styled.div`
   grid-template-columns: repeat(2, 1fr);
   grid-auto-rows: 90px;
   grid-gap: 24px;
+  justify-content: center;
+
+  @media (max-width: 750px) {
+    grid-template-columns: 90%;
+  }
+
+  @media (max-width: 550px) {
+    grid-template-columns: 90%;
+  }
 `
