80b07dad562315eb36b355b19035764ff831c3e9
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-11-15 17:11:27 -0300

    project grid scenes landing

--- src/pages/CenasLanding/index.tsx
+++ src/pages/CenasLanding/index.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react'
 import GoBack from '../../components/GoBack'
 
-import { Container } from './styles'
+import { Container, Wrapper, Header, Main, Footer } from './styles'
 
 const CenasLanding: FC = () => {
   return (
     <Container>
       <GoBack />
-      <h1>CenasLanding</h1>
+      <Wrapper>
+        <Header />
+        <Main />
+        <Footer />
+      </Wrapper>
     </Container>
   )
 }
--- src/pages/CenasLanding/styles.ts
+++ src/pages/CenasLanding/styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100vw;
+  height: 100%;
+`
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template:
+    'header header header' 100px
+    '...    main   ...' 100%
+    '...    footer ...' 80px
+    / 1fr 1100px 1fr;
+`
+
+export const Header = styled.header`
+  grid-area: header;
+  background-color: red;
+`
+
+export const Main = styled.main`
+  grid-area: main;
+  background-color: blue;
+`
+
+export const Footer = styled.footer`
+  grid-area: footer;
+  background-color: yellow;
+`
